5b09ded8dd87dbfa8c0d4adcca5db1f990754ce1
daniel <mendezamutarid@gmail.com>
2026-05-19 12:50:31 -0400

    Update frontend hosting

---
+++ frontend/compilacion-para-hosting/assets/VCardText-CCGpdqDo.js
@@ -0,0 +1,1 @@
+import{c as t}from"./VAvatar-Cq-DeKvl.js";const e=t("v-card-text");export{e as V};
---
+++ frontend/compilacion-para-hosting/assets/ssrBoot-tCq7dI1P.js
@@ -0,0 +1,1 @@
+import{aV as t,D as e,X as s,bK as n}from"./index-DXbUBCq9.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
